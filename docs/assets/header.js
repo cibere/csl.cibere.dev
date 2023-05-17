bda6efe6710ee9922578318e2f342bad7e9786b4
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-05-16 19:26:16 -0700

    me == dumb

--- docs/assets/header.js
+++ docs/assets/header.js
@@ -11,7 +11,7 @@ function addHeader(href){
                 <div class="dropdown-content">
                     <a href="/rules/discord"><button class="button${href === "discord_rules" ? " selected" : ""}" id="discord-rules-btn">Discord Rules</button></a>
                     <a href="/rules/game"><button class="button${href === "game_rules" ? " selected" : ""}" id="game-rules-btn">Game Rules</button></a>
-                    <a href="/rules/match"><button class="button${href === "match_rules" ? " selected" : ""}" id="match-rules-btn">Team Rules</button></a>
+                    <a href="/rules/match"><button class="button${href === "match_rules" ? " selected" : ""}" id="match-rules-btn">Match Rules</button></a>
                 </div>
             </div>
             <a target="__blank" href="https://discord.gg/YfNtky8Wnv"><button class="button">Discord</button></a>
